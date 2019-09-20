42588ec809cfe4c9e20f176abe4f70c176d3e1fa
Yong Ann Vouern <yongan007@gmail.com>
2019-09-21 02:04:28 +0300

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yong_ros/example_ws/src
+# Build directory: /home/yong_ros/example_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("description")
