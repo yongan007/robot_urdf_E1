fec7d61e0e92b6caf8c6d7fc407fca22d323f6cd
Yong Ann Vouern <yongan007@gmail.com>
2019-10-03 16:37:53 +0300

    modified

--- build/description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yong_ros/example_ws/src/description
-# Build directory: /home/yong_ros/example_ws/build/description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
