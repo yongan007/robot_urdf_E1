a77c4a38553d1a599704fd24be20f91e74a88c56
Yong Ann Vouern <yongan007@gmail.com>
2019-11-11 17:34:06 +0300

    add pictures

--- src/asimo_control/script/camera.py
+++ src/asimo_control/script/camera.py
@@ -1,6 +1,12 @@
+#! /usr/bin/python
+# Copyright (c) 2015, Rethink Robotics, Inc.
 
-#Modified from: https://gist.github.com/rethink-imcmahon/77a1a4d5506258f3dc1f
+# Using this CvBridge Tutorial for converting
+# ROS images to OpenCV2 images
+# http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
 
+# Using this OpenCV2 tutorial for saving Images:
+# http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html
 
 # rospy for the subscriber
 import rospy
@@ -10,9 +16,12 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 # OpenCV2 for saving an image
 import cv2
+import time
 
 # Instantiate CvBridge
 bridge = CvBridge()
+
+t0 =int(time.time())
 
 def image_callback(msg):
     print("Received an image!")
@@ -23,12 +32,23 @@ def image_callback(msg):
         print(e)
     else:
         # Save your OpenCV2 image as a jpeg 
-        cv2.imwrite('camera_image.jpeg', cv2_img)
+        # if t < 0:
+        t = str(int(time.time())-t0+1)
+        #print(t)
+        # else:
+        if int(t) <= 10:
+            cv2.imwrite('/home/yong_ros/example_ws/src/asimo_e1_description/Photos/camera_image'+t+'.jpeg', cv2_img)
+        else:
+            pass
+        
+        # else:
+        #     cv2.imwrite('camera_image.jpeg', cv2_img)
 
 def main():
+    
     rospy.init_node('image_listener')
     # Define your image topic
-    image_topic = "/cameras/left_hand_camera/image"
+    image_topic = "/asimo/camera1/image_raw"
     # Set up your subscriber and define its callback
     rospy.Subscriber(image_topic, Image, image_callback)
     # Spin until ctrl + c
